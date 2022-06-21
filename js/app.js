09fc9f51168173b7068b31547c5e423d147c6e5d
kmnb6842 <kmnb6842@gmail.com>
2022-06-21 15:19:04 +0800

    22222

--- js/app.js
+++ js/app.js
@@ -10,20 +10,36 @@
 // });
 
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    var show = document.querySelectorAll(".show");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 300;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");      
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  var reveals = document.querySelectorAll(".reveal");
+  var show = document.querySelectorAll(".show");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 300;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
-  window.addEventListener("scroll", reveal);
-  
-  // To check the scroll position on page load
-  reveal();
+}
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
+
+$(window).on("scroll touchmove", function () {
+  if ($(document).scrollTop() >= $("#Service01").position().top && $(document).scrollTop() < $("#Service02").position().top) {
+    $('body').css('background-image', 'url(img/img_one.jpg)')
+  };
+  if ($(document).scrollTop() >= $("#two").position().top && $(document).scrollTop() < $("#three").position().top) {
+    $('body').css('background-image', 'url(img/img_two.jpg)')
+  };
+  if ($(document).scrollTop() >= $("#three").position().top && $(document).scrollTop() < $("#four").position().top) {
+    $('body').css('background-image', 'url(img/img_three.jpg)')
+  };
+  if ($(document).scrollTop() >= $("#four").position().top) {
+    $('body').css('background-image', 'url(img/img_four.jpg)')
+  };
+
+});
