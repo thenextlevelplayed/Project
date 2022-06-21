d7f044d902047301443057ba5a5abba66bdeaab7
Tsaimingming <d111111guy@gmail.com>
2022-06-21 15:11:18 +0800

    0621

---
+++ indexanime/index.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', () => {
+    const kaimau = document.querySelector('.kaimau');
+    const kaimauTitle = document.querySelector('.kaimau__title');
+    const kaimauCover = document.querySelector('.kaimau__cover');
+
+    let kaimauHeight = kaimau.clientHeight;
+    let scaleRatio = Math.pow(400, 1/kaimauHeight);
+
+    window.addEventListener('scroll', () => {
+        let scrollPosition = window.scrollY;
+        let scaleAmount = Math.pow(scaleRatio, scrollPosition);
+        
+        if(scrollPosition > 0) {
+            kaimauCover.classList.add('hidden');
+        } else {
+            kaimauCover.classList.remove('hidden');
+        }
+
+        if(scaleAmount < 200) {  
+            kaimauTitle.style.transform = `scale(${scaleAmount})`;
+        } else {
+            kaimauTitle.style.transform = `scale(400)`;
+        }
+    });
+});
+
+// let k = document,querySelector('#k');
+// window.addEventListener('scroll',function(){
+//     let value = window.scrollY;
+//     k.style.backgroundSize = 100 + value*2 + "px";
+// )}
