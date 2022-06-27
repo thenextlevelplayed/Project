e84593877d9985f5b18a22499579688710d502bc
kmnb6842 <kmnb6842@gmail.com>
2022-06-27 21:55:26 +0800

    123

--- js/app.js
+++ js/app.js
@@ -9,6 +9,9 @@
 //     })
 // });
 
+
+//首頁圖片動畫
+
 $(window).on("scroll touchmove", function () {
   if ($(document).scrollTop() < (($("#Service01").position().top)+1700)) {
     console.log($("#Service01").position().top);
@@ -61,3 +64,28 @@ $(window).on("scroll touchmove", function () {
   }
 
 );
+
+//navbar動畫
+
+
+  $(function () {
+    $('#active').on('click', function () {
+        // console.log($('#active').prop('checked'));
+        if ($('#active').prop('checked') == false) {
+            $('.wapperContentWord a').css('left', '500px');
+        } else {
+            setTimeout(function () {
+                $('.wapperContentWord a:nth-child(1)').css('left', '0px');
+            }, 300);
+            setTimeout(function () {
+                $('.wapperContentWord a:nth-child(3)').css('left', '0px');
+            }, 350);
+            setTimeout(function () {
+                $('.wapperContentWord a:nth-child(5)').css('left', '0px');
+            }, 400);
+            setTimeout(function () {
+                $('.wapperContentWord a:nth-child(7)').css('left', '0px');
+            }, 450);
+        }
+    })
+})
