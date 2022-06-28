44154d75df996923536615e7829205318f493b01
SwenChuang <a159856a@gmail.com>
2022-06-28 17:24:22 +0800

    0628 delete

--- new animetion/ia2.js
+++
@@ -1,8 +0,0 @@
-var img = document.querySelector("#img");
-console.log(img.style.clipPath)
-
-window.addEventListener("scroll",function(){
-    var scroll = window.scrollY;
-
-    img.style.clipPath = "circle("+scroll+" px at center)"
-})
