58606c196e9210d87c01aba8ef94067420baea48
Tsaimingming <d111111guy@gmail.com>
2022-06-28 17:18:56 +0800

    0628

--- new animetion/ia2.js
+++ new animetion/ia2.js
@@ -1,5 +1,8 @@
-var img = document.querySelector(".kaimau-image");
+var img = document.querySelector("#img");
+console.log(img.style.clipPath)
+
 window.addEventListener("scroll",function(){
     var scroll = window.scrollY;
+
     img.style.clipPath = "circle("+scroll+" px at center)"
 })
