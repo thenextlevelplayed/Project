62c27725bb09a51332aaba310f79a2b0918690ee
kmnb6842 <kmnb6842@gmail.com>
2022-06-28 17:14:10 +0800

    delete ia2

--- new animetion/ia2.js
+++
@@ -1,5 +0,0 @@
-var img = document.querySelector(".kaimau-image");
-window.addEventListener("scroll",function(){
-    var scroll = window.scrollY;
-    img.style.clipPath = "circle("+scroll+" px at center)"
-})
