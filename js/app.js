ccb61931aa6eb7687da23b0264fbdc0c88794f45
kmnb6842 <kmnb6842@gmail.com>
2022-07-03 17:23:14 +0800

    0703

--- js/app.js
+++ js/app.js
@@ -100,25 +100,5 @@ $(window).on("scroll touchmove", function () {
 //navbar動畫
 
 
-  $(function () {
-    $('#active').on('click', function () {
-        // console.log($('#active').prop('checked'));
-        if ($('#active').prop('checked') == false) {
-            $('.wapperContentWord a').css('left', '500px');
-        } else {
-            setTimeout(function () {
-                $('.wapperContentWord a:nth-child(1)').css('left', '0px');
-            }, 300);
-            setTimeout(function () {
-                $('.wapperContentWord a:nth-child(3)').css('left', '0px');
-            }, 350);
-            setTimeout(function () {
-                $('.wapperContentWord a:nth-child(5)').css('left', '0px');
-            }, 400);
-            setTimeout(function () {
-                $('.wapperContentWord a:nth-child(7)').css('left', '0px');
-            }, 450);
-        }
-    })
-})
+
 
---
+++ js/navbar.js
@@ -0,0 +1,21 @@
+$(function () {
+    $('#active').on('click', function () {
+        // console.log($('#active').prop('checked'));
+        if ($('#active').prop('checked') == false) {
+            $('.wapperContentWord a').css('left', '500px');
+        } else {
+            setTimeout(function () {
+                $('.wapperContentWord a:nth-child(1)').css('left', '0px');
+            }, 300);
+            setTimeout(function () {
+                $('.wapperContentWord a:nth-child(3)').css('left', '0px');
+            }, 350);
+            setTimeout(function () {
+                $('.wapperContentWord a:nth-child(5)').css('left', '0px');
+            }, 400);
+            setTimeout(function () {
+                $('.wapperContentWord a:nth-child(7)').css('left', '0px');
+            }, 450);
+        }
+    })
+})
