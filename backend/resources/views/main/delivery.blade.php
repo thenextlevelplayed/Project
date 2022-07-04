72d5614d9a1d18366a85e77e95f47b07de82782a
thenextlevelplayed <kitty93213@gmail.com>
2022-07-04 13:33:28 +0800

    delivery

--- backend/resources/views/main/delivery.blade.php
+++ backend/resources/views/main/delivery.blade.php
@@ -9,10 +9,55 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.5/uxl/popper.min.js"></script>
     <script src="https://cdn.usebootstrap.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
+    <link rel="stylesheet" href="/css/delivery.css">
     <link rel="stylesheet" href="https://cdn.usebootstrap.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 <body>
+
+    <div class="jumbotron">
+        
+        <table class="table" style="">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>出貨單編號</th>
+                    <th>客戶名稱</th>
+                    <th>編輯</th>
+                    <th>狀態</th>
+                </tr>		  
+            </thead>
+
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>KMD20220623001</td>
+                    <td>華碩</td>
+                    <td><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg></td>
+                    <td class="btn btn-warning">未出貨</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>KMD20220623001</td>
+                    <td>華碩</td>
+                    <td ><svg class="btn" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg></td>
+                    <td class="btn btn-warning">未出貨</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>KMD20220623001</td>
+                    <td>華碩</td>
+                    <td><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg></td>
+                    <td class="btn btn-success">已出貨</td>
+                </tr>
+                <tr>
+                    <th scope="row">4</th>
+                    <td>KMD20220623001</td>
+                    <td>iSpan</td>
+                    <td><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg></td>
+                    <td class="btn btn-success">已出貨</td>
+                </tr>
+            </tbody>
+        </table>
     <i class="fa fa-edit"></i>
     
 </body>
