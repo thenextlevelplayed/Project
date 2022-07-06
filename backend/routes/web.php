ae95049df34d9354d22e5211b7fa8a6659aa3bb3
SwenChuang <a159856a@gmail.com>
2022-07-06 09:26:07 +0800

    lalalalalala

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -39,6 +39,7 @@ Route::get('/main', "App\Http\Controllers\BackendController@index");
 //進銷存
 Route::get('/main/erp', "App\Http\Controllers\BackendController@erp");
 Route::get('/main/purchase', "App\Http\Controllers\BackendController@purchase");
+Route::get('/main/purchaseCreate', "App\Http\Controllers\BackendController@purchaseCreate");
 Route::get('/main/sales', "App\Http\Controllers\BackendController@sales");
 Route::get('/main/stock', "App\Http\Controllers\BackendController@stock");
 
