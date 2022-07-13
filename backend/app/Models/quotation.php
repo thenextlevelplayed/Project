5b0c85c770c748835ccf679e035e6e8343f1cbd8
thenextlevelplayed <kitty93213@gmail.com>
2022-07-14 00:20:40 +0800

    database

---
+++ backend/app/Models/quotation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class quotation extends Model
+{
+use HasFactory;
+    public $fillable = 
+        [
+            'qid',
+            'qdate',
+            'cid',
+            'qContact',
+            'cmail',
+            'staffid',
+            'dlid',
+            'rid',
+            'qstatus'
+        ];
+    }
