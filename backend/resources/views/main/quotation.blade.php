16bc9c092243f0aab0a7e1b0a9c6f7236bb67277
hey2804 <cc851128@gmail.com>
2022-07-15 00:41:26 +0800

    0715

--- backend/resources/views/main/quotation.blade.php
+++ backend/resources/views/main/quotation.blade.php
@@ -44,113 +44,31 @@
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td>
-                                        <a href="/main/quotation/{quotationId}">KMQ20220623001</a>                                                    
-                                    </td>
-                                    <td>
-                                        華碩
-                                    </td>
-                                    <td>
-                                        KMO20220624001
-                                    </td>
-                                    <td>
-                                        <span class="badge bg-success">
-                                            已成立訂單
-                                        </span> 
-                                    </td>
-                                    <td class="btn btn-link">
-                                        <a href="/quotation/edit/1">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                        </svg>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        KMQ20220623002
-                                    </td>
-                                    <td>
-                                        iSpan
-                                    </td>
-                                    <td>
-                                        KMO20220624002
-                                    </td>
-                                    <td>
-                                        <span class="badge bg-success">
-                                            已成立訂單
-                                        </span> 
-                                    </td>
-                                    <td class="btn btn-link">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                    @foreach ($quotation as $q)
+                                        <td>
+                                            <a href="/main/quotation/{quotationId}">{{$q->qid}}</a>                                                    
+                                        </td>
+                                        <td>
+                                            {{$q->cid}}
+                                        </td>
+                                        <td>
+                                            KMO20220624001
+                                        </td>
+                                        <td>
+                                            <span class="badge bg-success">
+                                                已成立訂單
+                                            </span> 
+                                        </td>
+                                        <td class="btn btn-link">
+                                            <a href="/quotation/edit/1">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                             </svg>
-                                    </td>
+                                        </td>
+                                    @endforeach
                                 </tr>
-                                <tr>
-                                    <td>
-                                        KMQ20220623003
-                                    </td>
-                                    <td>
-                                        華碩
-                                    </td>
-                                    <td>
-                                    </td>
-                                    <td>
-                                        <span class="badge bg-danger">
-                                            未成立訂單
-                                        </span> 
-                                    </td>
-                                    <td class="btn btn-link">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                            </svg>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        KMQ20220624001
-                                    </td>
-                                    <td>
-                                        Dell
-                                    </td>
-                                    <td>
-                                        KMO20220625001
-                                    </td>
-                                    <td>
-                                        <span class="badge bg-success">
-                                            已成立訂單
-                                        </span> 
-                                    </td>
-                                    <td class="btn btn-link">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                            </svg>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        KMQ20220624001
-                                    </td>
-                                    <td>
-                                        Dell
-                                    </td>
-                                    <td></td>
-                                    <td>
-                                        <span class="badge bg-danger">
-                                            未成立訂單
-                                        </span> 
-                                    </td>
-                                    <td class="btn btn-link">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                            </svg>
-                                    </td>
-                                </tr>
+                                
                             </tbody>
                         </table>
                     </div>
