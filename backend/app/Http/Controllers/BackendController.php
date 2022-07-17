9a6c50fd3bdfe828d406c39b81fb3b8640c42c9a
kmnb6842 <kmnb6842@gmail.com>
2022-07-17 21:45:04 +0800

    search

--- backend/app/Http/Controllers/BackendController.php
+++ backend/app/Http/Controllers/BackendController.php
@@ -116,6 +116,7 @@ class BackendController extends Controller
         // dd($quotation);
         return view('main.quotation', compact('quotation'));
     }    
+
     function quotationInfo($quotationId)
     {
         //報價資訊
@@ -155,6 +156,22 @@ class BackendController extends Controller
         ->select('*')
         ->get();
 
+        $search_text = $_GET['query'] ?? "";
+        if ($search_text != ""){
+            $order = Order::join('quotation', 'quotation.qid', '=', 'order.oid')
+            ->join('detaillist', 'detaillist.dlid', '=', 'quotation.dlid')
+            ->join('customer','customer.cid','=','quotation.cid')
+            ->where('cname','LIKE','%'.$search_text.'%')
+            ->orWhere('oid','LIKE','%'.$search_text.'%')
+            ->get();
+            
+        }
+        else{
+            $order = Order::join('quotation', 'quotation.qid', '=', 'order.oid')
+            ->join('detaillist', 'detaillist.dlid', '=', 'quotation.dlid')
+            ->join('customer','customer.cid','=','quotation.cid')
+            ->get();                        
+        };
         return view('main.order', compact('order'));
     }
     function orderInfo(){
@@ -195,7 +212,7 @@ class BackendController extends Controller
         ->get();
         
 
-        $search_text = $_GET['query'] ?? "";
+        $search_text = $_GET['query'] ?? ""; //判斷第一個變數有沒有存在，若沒有則回傳空字串
         if ($search_text != ""){
             $manufacture = Manufacture::join('order','order.oid','=','manufacture.oid')
             ->join('quotation','quotation.qid','=','order.qid')
@@ -221,7 +238,7 @@ class BackendController extends Controller
     }
     function manufactureEdit($manufactureId)
     {
-        // 製造
+        // 製造編輯
 
         $manufactureedit = Manufacture::join('order','order.oid','=','manufacture.oid')
         ->join('quotation','quotation.qid','=','order.qid')
@@ -234,32 +251,6 @@ class BackendController extends Controller
         return view('manufacture.manufactureEdit',["manu"=>$manufactureedit]);
     }
 
-    public function manuSearch(){
-
-        $search_text = $_GET['query'];
-        if ($search_text != ""){
-            $manufacture = Manufacture::join('order','order.oid','=','manufacture.oid')
-            ->join('quotation','quotation.qid','=','order.qid')
-            ->join('customer','customer.cid','=','quotation.cid')
-            ->join('detaillist','detaillist.dlid','=','quotation.dlid')
-            ->where('cname','LIKE','%'.$search_text.'%')
-            ->orWhere('detaillist.dlid','LIKE','%'.$search_text.'%')
-            ->orWhere('mid','LIKE','%'.$search_text.'%')
-            ->get();
-        }
-        else{
-            $manufacture = Manufacture::join('order','order.oid','=','manufacture.oid')
-            ->join('quotation','quotation.qid','=','order.qid')
-            ->join('customer','customer.cid','=','quotation.cid')
-            ->join('detaillist','detaillist.dlid','=','quotation.dlid')
-            ->select('*')
-            ->get();
-            
-        };
-
-        return view('manufacture.search',["manufacture"=>$manufacture]);
-
-    }
 
     function delivery(Request $request)
     {
@@ -267,6 +258,7 @@ class BackendController extends Controller
         // 接上一張表主鍵的表,上張表主鍵,'=',目前這張表和上一張相同主鍵
         $d = Delivery::join('manufacture', 'manufacture.mid', '=', 'delivery.mid')
             ->join('order', 'order.oid', '=', 'manufacture.oid')
+            
             ->select('*')
             ->get();
 
