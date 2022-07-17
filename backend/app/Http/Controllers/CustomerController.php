2ec71e0f1bb17e446c211c8e03dc82c5272eb131
thenextlevelplayed <kitty93213@gmail.com>
2022-07-18 01:11:00 +0800

    RW customer plus database

--- backend/app/Http/Controllers/CustomerController.php
+++ backend/app/Http/Controllers/CustomerController.php
@@ -23,27 +23,38 @@ class CustomerController extends Controller
 
     function customerInfo($customerId)
     {
-
         //檢視客戶
-        return view('customer.customerInfo');
+        $customerInfo = Customer::find($customerId);
+        return view('customer.customerInfo',compact('customerInfo'));
     }
 
-    function customerInfoEdit(Request $request)
+    function customerInfoEdit(Request $request,$customerId)
     {
-
         //編輯客戶
-        $var1 = $request->input('var1');
-        $var2 = $request->input('var2');
-        return view('customer.customerEdit');
+        $customerInfo = Customer::find($customerId);
+        return view('customer.customerEdit',compact('customerInfo'));
     }
 
-    function customerUpdate(Request $request)
+    function customerUpdate(Request $request,$customerId)
     {
-
         //更新客戶資料
-        $var1 = $request->input('var1');
-        $var2 = $request->input('var2');
-        return view('customer.customerInfo');
+        $customerInfo = Customer::find($customerId);
+        $customerInfo->cid = $request->input('cid');
+        $customerInfo->cname = $request->input('cname');
+        $customerInfo->ctel = $request->input('ctel');
+        $customerInfo->cmail = $request->input('cmail');
+        $customerInfo->caddress = $request->input('caddress');
+        $customerInfo->tradecode = $request->input('tradecode');
+        $customerInfo->legalletter = $request->input('legalletter');
+        $customerInfo->director = $request->input('director');
+        $customerInfo->fax = $request->input('fax');
+        $customerInfo->salesrep = $request->input('salesrep');
+        $customerInfo->instruments = $request->input('instruments');
+        $customerInfo->location = $request->input('location');
+        $customerInfo->clineid = $request->input('clineid');
+        $customerInfo->save(); //儲存
+
+        return view('customer.customerInfo',compact('customerInfo'));
     }
 
     function customerAdd(Request $request)
--- backend/resources/views/customer/customerInfo.blade.php
+++ backend/resources/views/customer/customerInfo.blade.php
@@ -36,7 +36,7 @@
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>*統一編號</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" required>
+                                                <input type="text" value="{{$customerInfo->cid}}" name="cid" required>
                                             </div>
                                         </div>
                                     
@@ -44,32 +44,38 @@
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>*交易暗語</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" required>
+                                                <input type="text" value="{{$customerInfo->tradecode}}" name="tradecode" required>
                                             </div>
                                         </div>
                                         
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>*開立形式</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" required>
+                                                <input type="text"  value="{{$customerInfo->legalletter}}" name="legalletter" required>
                                             </div>
                                         </div>
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>公司負責人</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" class="" required>
+                                                <input type="text"  value="{{$customerInfo->director}}" name="director" required>
                                             </div>
                                         </div>
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>公司傳真</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" class="" required>
+                                                <input type="text"  value="{{$customerInfo->fax}}" name="fax" required>
                                             </div>
                                         </div>
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>業務負責人</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" class="" required>
+                                                <input type="text"  value="{{$customerInfo->salesrep}}" name="salesrep" required>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-1">
+                                            <div class="col-lg-3"><p>LineID</p></div>
+                                            <div class="col-lg-8">
+                                                <input type="text"  value="{{$customerInfo->clineid}}" name="clineid" required>
                                             </div>
                                         </div>
                                     </fieldset>
@@ -79,37 +85,37 @@
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>*公司名稱</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" class="" required>
+                                                <input type="text" value="{{$customerInfo->cname}}" name="cname" required>
                                             </div>
                                         </div>
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>*交易對象</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" class="" required>
+                                                <input type="text" value="{{$customerInfo->instruments}}" name="instruments"required>
                                             </div>
                                         </div>
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>*公司信箱</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" class="" required>
+                                                <input type="text" value="{{$customerInfo->cmail}}" name="cmail"required>
                                             </div>
                                         </div>
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>公司電話</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" class="" required>
+                                                <input type="text" value="{{$customerInfo->ctel}}" name="ctel"required>
                                             </div>
                                         </div>
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>公司地址</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" class="" required>
+                                                <input type="text" value="{{$customerInfo->caddress}}" name="caddress"required>
                                             </div>
                                         </div>
                                         <div class="row mb-1">
                                             <div class="col-lg-3"><p>通訊地址</p></div>
                                             <div class="col-lg-8">
-                                                <input type="text" class="" required>
+                                                <input type="text" value="{{$customerInfo->location}}" name="location"required>
                                             </div>
                                         </div>
                                     </fieldset>
