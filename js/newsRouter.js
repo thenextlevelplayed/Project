5cf575e89383535790879bce5b4999665a20c173
kmnb6842 <kmnb6842@gmail.com>
2022-07-19 02:51:31 +0800

    vue router

---
+++ js/newsRouter.js
@@ -0,0 +1,373 @@
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const lenovo = {
+    template: `
+<div class="container-fluid">
+    <div class="bg-light p-3">
+        <div class="row Cooperate">
+            <div class="col-lg-6 rec300 ">
+                <div class="center">
+                    <img class="w-75 h-auto" src="../資訊圖庫/合作夥伴logo/LENOVO-1.jpg" alt="補圖" />
+                    <h2 class="cooperateTitle">LENOVO</h2>
+                </div>
+            </div>
+            <div class="col-lg-6 pl-4">
+                <div class="row cooperateInfo">
+                    <div>
+                        <p>商用桌機</p>
+                    </div>
+                    <div>
+                        <p>商用筆電</p>
+                    </div>
+                    <div>
+                        <p>顯示器</p>
+                    </div>
+                    <div>
+                        <p>伺服器</p>
+                    </div>
+                    <div>
+                        <p>工作站</p>
+                    </div>
+                    <div>
+                        <p>STORAGE</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`}
+
+const microsoft = {
+    template: `
+<div class="container-fluid">
+<div class="bg-light p-3">
+    <div class="row Cooperate">
+        <div class="col-lg-6 rec300 ">
+            <div class="center">
+                <img class="w-75 h-auto" src="../資訊圖庫/合作夥伴logo/MICROSOFT-1.png" alt="補圖" />
+                <h2 class="cooperateTitle">Microsoft</h2>
+            </div>
+        </div>
+        <div class="col-lg-6 pl-4">
+            <div class="row cooperateInfo">
+                <div>
+                    <p>作業系統</p>
+                </div>
+                <div>
+                    <p>授權軟體</p>
+                </div>
+                <div>
+                    <p>辦公軟體</p>
+                </div>
+                <div>
+                    <p>資料庫</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`}
+
+const asus = {
+    template: `
+<div class="container-fluid">
+<div class="bg-light p-3">
+    <div class="row Cooperate">
+        <div class="col-lg-6 rec300 ">
+            <div class="center">
+                <img class="w-75 h-auto" src="../資訊圖庫/合作夥伴logo/ASUS.png" alt="補圖" />
+                <h2 class="cooperateTitle">ASUS</h2>
+            </div>
+        </div>
+        <div class="col-lg-6 pl-4">
+            <div class="row cooperateInfo">
+                <div>
+                    <p>商用桌機</p>
+                </div>
+                <div>
+                    <p>商用筆電</p>
+                </div>
+                <div>
+                    <p>顯示器</p>
+                </div>
+                <div>
+                    <p>主機零組件</p>
+                </div>
+                <div>
+                    <p>網通產品</p>
+                </div>
+                <div>
+                    <p>伺服器</p>
+                </div>
+                <div>
+                    <p>工作站</p>
+                </div>
+                <div>
+                    <p>ASUS CONTROL CENTER</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`}
+
+const dell = {
+    template: `
+<div class="container-fluid">
+<div class="bg-light p-3">
+    <div class="row Cooperate">
+        <div class="col-lg-6 rec300 ">
+            <div class="center">
+                <img class="w-75 h-auto" src="../資訊圖庫/合作夥伴logo/Dell_Logo.png" alt="補圖" />
+                <h2 class="cooperateTitle">DELL</h2>
+            </div>
+        </div>
+        <div class="col-lg-6 pl-4">
+            <div class="row cooperateInfo">
+                <div>
+                    <p>VxRail超融合</p>
+                </div>
+                <div>
+                    <p>商用桌機</p>
+                </div>
+                <div>
+                    <p>商用筆電</p>
+                </div>
+                <div>
+                    <p>顯示器</p>
+                </div>
+                <div>
+                    <p>伺服器</p>
+                </div>
+                <div>
+                    <p>工作站</p>
+                </div>
+                <div>
+                    <p>STORAGE</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`}
+
+const checkpoint = {
+    template: `
+<div class="container-fluid">
+<div class="bg-light p-3">
+    <div class="row Cooperate">
+        <div class="col-lg-6 rec300 ">
+            <div class="center">
+                <img class="w-75 h-auto" src="../資訊圖庫/合作夥伴logo/checkpoint-logo-stacked-large.png" alt="補圖" />
+                <h2 class="cooperateTitle">Check Point</h2>
+            </div>
+        </div>
+        <div class="col-lg-6 pl-4">
+            <div class="row cooperateInfo">
+                <div>
+                    <p>FIREWALL</p>
+                </div>
+                <div>
+                    <p>Harmony</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`}
+
+const HPE = {
+    template: `
+<div class="container-fluid">
+<div class="bg-light p-3">
+    <div class="row Cooperate">
+        <div class="col-lg-6 rec300 ">
+            <div class="center">
+                <img class="w-75 h-auto" src="../資訊圖庫/合作夥伴logo/Hewlett_Packard_Enterprise_logo.svg.png" alt="補圖" />
+                <h2 class="cooperateTitle">HPE</h2>
+            </div>
+            
+        </div>
+        <div class="col-lg-6 pl-4">
+            <div class="row cooperateInfo">
+                <div>
+                    <p>超融合(SIMPLIVITY)</p>
+                </div>
+                <div>
+                    <p>伺服器</p>
+                </div>
+                <div>
+                    <p>STORAGE</p>
+                </div>
+                <div>
+                    <p>商用桌機</p>
+                </div>
+                <div>
+                    <p>商用筆電</p>
+                </div>
+                <div>
+                    <p>工作站</p>
+                </div>
+                <div>
+                    <p>網路設備</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`}
+
+const cisco = {
+    template: `
+<div class="container-fluid">
+<div class="bg-light p-3">
+    <div class="row Cooperate">
+        <div class="col-lg-6 rec300">
+            <div class="center">
+                <img class="w-50 h-auto" src="../資訊圖庫/合作夥伴logo/Cisco-logo.png"/>
+                <h2 class="cooperateTitle">CISCO</h2>
+            </div>
+           
+        </div>
+        <div class="col-lg-6 pl-4">
+            <div class="row cooperateInfo">
+                <div>
+                    <p>HyperFlex</p>
+                </div>
+                <div>
+                    <p>防火牆</p>
+                </div>
+                <div>
+                    <p>WEBEX</p>
+                </div>
+                <div>
+                    <p>網路設備</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`}
+
+const aruba = {
+    template: `
+<div class="container-fluid">
+<div class="bg-light p-3">
+    <div class="row Cooperate">
+        <div class="col-lg-6 rec300">
+            <div class="center">
+                <img class="w-75 h-auto" src="../資訊圖庫/合作夥伴logo/aruba.svg"/>
+                <h2 class="cooperateTitle">ARUBA</h2>
+            </div>
+        </div>
+        <div class="col-lg-6 pl-4">
+            <div class="row cooperateInfo">
+                <div>
+                    <p>SWITCH</p>
+                </div>
+                <div>
+                    <p>控制器</p>
+                </div>
+                <div>
+                    <p>AP</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`}
+
+const fortinet = {
+    template: `
+<div class="container-fluid">
+<div class="bg-light p-3">
+    <div class="row Cooperate">
+        <div class="col-lg-6 rec300 ">
+            <div class="center">
+                <img class="w-75 h-auto" src="../資訊圖庫/合作夥伴logo/Fortinet-Logo.png"/>
+                <h2 class="cooperateTitle">Fortinet</h2>
+            </div>
+        </div>
+        <div class="col-lg-6 pl-4">
+            <div class="row cooperateInfo">
+                <div>
+                    <p>防火牆</p>
+                </div>
+                <div>
+                    <p>SWITCH</p>
+                </div>
+                <div>
+                    <p>AP</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`}
+
+const DVC = {
+    template: `
+<div class="container-fluid">
+<div class="bg-light p-3">
+    <div class="row Cooperate">
+        <div class="col-lg-6 rec300">
+            <div class="center">
+                <img class="w-50 h-auto" src="../資訊圖庫/合作夥伴logo/DVC.svg"/>
+                <h2 class="cooperateTitle">DVC</h2>
+            </div>
+            
+        </div>
+        <div class="col-lg-6 pl-4">
+            <div class="row cooperateInfo">
+                <div>
+                    <p>文件加密保險系統</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`}
+
+const dd = {
+    template: `
+<div class="container-fluid">
+<div class="bg-light p-3">
+    <div class="row Cooperate">
+        <div class="col-lg-12 rec300">
+            <div class="center">
+                <h2 class="cooperateDefault">點選上方LOGO顯示內容</h2>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`}
+
+
+
+
+
+
+const routes = [
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar },
+    { path: '/lenovo', component: lenovo },
+    { path: '/microsoft', component: microsoft },
+    { path: '/asus', component: asus },
+    { path: '/dell', component: dell },
+    { path: '/checkpoint', component: checkpoint },
+    { path: '/HPE', component: HPE },
+    { path: '/cisco', component: cisco },
+    { path: '/aruba', component: aruba },
+    { path: '/fortinet', component: fortinet },
+    { path: '/DVC', component: DVC },
+    { path: '', component: dd },
+
+]
+const router = new VueRouter({
+    routes
+})
+const app = new Vue({
+    router
+}).$mount('#app')
