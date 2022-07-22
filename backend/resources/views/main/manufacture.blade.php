1f2af48e56a59a53fb472d1d776c4086dbbd3c6e
kmnb6842 <107172404+kmnb6842@users.noreply.github.com>
2022-07-22 16:28:30 +0800

    123

--- backend/resources/views/main/manufacture.blade.php
+++ backend/resources/views/main/manufacture.blade.php
@@ -21,7 +21,7 @@
     <div class="content">
         <div class="row">
             <div class="col-md-12">
-                <div class="card">
+                <div class="card p-3">
                     <div class="card-body">
                         <div class="table-responsive">
                             <table class="table">
