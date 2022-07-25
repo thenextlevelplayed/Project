1023c09f24738a375d8e72c55836eb31338c7860
kmnb6842 <kmnb6842@gmail.com>
2022-07-26 03:18:34 +0800

    pdf view

--- backend/resources/views/pdf/manufactureEdit.blade.php
+++ backend/resources/views/pdf/manufactureEdit.blade.php
@@ -12,130 +12,88 @@
     body {
     font-family: NotoSansTC-Regular, DejaVu Sans,sans-serif;
     border: 0;
+
 }
 </style>
 
-<div class="content">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="container p-5">
-                        <div>
-                            <div class="col-md-12 card-title text-center">
-                                <p>凱茂資訊 工單管理</p>
-                            </div>
-                            <div class="row ">
-                                <div class="col-md-6 mb-3">
-                                    <label for="validationCustom01">工單編號</label>
-                                    <input class="form-control" type="text" placeholder="666" readonly>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="validationCustom01">客戶聯絡人</label>
-                                    <input class="form-control" type="text" placeholder="666" readonly>
-                                </div>
-                            </div>
-                            <div class="row ">
-                                <div class="col-md-6 mb-3">
-                                    <label for="validationCustom01">客戶名稱</label>
-                                    <input class="form-control" type="text" placeholder="666" readonly>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="validationCustom01">客戶信箱</label>
-                                    <input class="form-control" type="text" placeholder="" readonly>
-                                </div>
-                            </div>
-                            <div class="row ">
-                                <div class="col-md-6 mb-3">
-                                    <label for="validationCustom01">客戶統編</label>
-                                    <input class="form-control" type="text" placeholder="" readonly>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="validationCustom01">工單成立日期</label>
-                                    <input class="form-control" type="text" placeholder="" readonly>
-                                </div>
-                            </div>
-                            <div class="row ">
-                                <div class="col-md-12 mb-3">
-                                    <label for="exampleFormControlTextarea1">工單備註</label>
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="1">客戶施工處危險需注意安全</textarea>
-                                </div>
-                            </div>
-                        </div>
+<body>
+    <div class="container">
+        <div class="col-md-12 card-title text-center">
+            <p style="font-size:30px">楷模資訊 工單管理</p>
+        </div>
 
-                        <div class="table-responsive">
-                            <div class="col-md-12 card-title text-center">
-                                <p>工單商品內容</p>
-                            </div>
-                            <table class="table table-hover">
-                                <thead class="text-primary" style="white-space: nowrap">
-                                    <td>
-                                        商品名稱
-                                    </td>
-                                    <td>
-                                        商品型號
-                                    </td>
-                                    <td>
-                                        商品數量
-                                    </td>
-                                    <td>
-                                        商品備註
-                                    </td>
-                                    <td>
-                                        完成
-                                    </td>
-
-                                </thead>
-                                <tbody>
-                                    
-                                    <tr>
-                                        <td>
-                                            123
-                                        </td>
-                                        <td>
-                                            123
-                                        </td>
-                                        <td>
-                                            123
-                                        </td>
-                                        <td>
-                                            <div style="width:300px">
-                                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="1">電腦超重需要使用推車</textarea>
-                                            </div>
-                                        </td>
-                                        <td style = "text-align: center">
-                                            <input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="">
-                                        </td>
-                                    </tr>
-                                    
-
-                                </tbody>
-                            </table>
-                        </div>
-
-                        {{-- <div class = "container-fluid border">
-                            <br>
-                            <br>
-                            
-                        </div> --}}
-                        <div class="row">
-                            <div class="col-md-6 mb-1 bg-light p-4 border border-white">
-                                企業方案：<br>
-                                週年慶滿千送百
-                            </div>
-                            <div class="col-md-6 mb-1 bg-light p-4 border border-white">
-                                業務專員：林小姐<br>
-                                凱茂信箱：km2468@gmail.com
-                            </div>
-                        </div>
-                    
-                    </div>
-
-                </div>
-
+        <div class="row">
+            <div class="col-md-12">
+                <hr>
+                工單編號:{{ $manu->mid }}
             </div>
+            <div class="col-md-12">
+                客戶聯絡人:{{ $manu->qcontact }}
+            </div>
+            <div class="col-md-12">
+                客戶名稱:{{ $manu->cname }}
+            </div>
+            <div class="col-md-12">
+                客戶信箱:{{ $manu->cmail }}
+            </div>
+            <div class="col-md-12">
+                客戶統編:{{ $manu->cid }}
+            </div>
+            <div class="col-md-12">
+                工單備註:<br>
+                {{$manu->mremark}}
+                <hr>
+            </div>
+        </div>
 
+        <div class="text-center">
+            <p style="font-size:20px">工單商品內容</p>
+        </div>
+        <table class="table">
+            <thead class="text-primary">
+                <td>
+                    商品名稱
+                </td>
+                <td>
+                    商品型號
+                </td>
+                <td>
+                    商品數量
+                </td>
+                <td>
+                    商品備註
+                </td>
+
+
+            </thead>
+            <tbody>
+                @foreach ($dtl as $key => $item)
+                <tr>
+                    <td>
+                        {{ $item->mname }}
+                    </td>
+                    <td>
+                        {{ $item->mnumber }}
+                    </td>
+                    <td>
+                        {{ $item->quantity }}
+                    </td>
+                    <td>
+                        <p>{{ $item->remark }}</p>
+
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <br>
+        <br>
+        <br>
+        <div class="container-fluid">
+            <p style="text-align:right">客戶簽名:_____________</p>
 
         </div>
+
+
     </div>
-</div>
+</body>
